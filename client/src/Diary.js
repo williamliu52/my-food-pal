e1e15388138b3e1839984853aad4a2fd014ed8fe
Will Liu <will.r.liu@gmail.com>
2017-09-13 01:03:35 -0400

    Add date along with forward/back buttons.

--- client/src/Diary.js
+++ client/src/Diary.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
 
 class Diary extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeDate: new Date()
+        }
+    }
+
+    handleBack = function (e) {
+        this.setState({
+            activeDate: new Date(this.state.activeDate.setDate(this.state.activeDate.getDate()-1))
+        })
+    }
+    handleForward = function (e) {
+        this.setState({
+            activeDate: new Date(this.state.activeDate.setDate(this.state.activeDate.getDate()+1))
+        })
+    }
+
     render() {
         return (
             <div className='col-md-4'>
-                Hello
+                <Button onClick={this.handleBack.bind(this)}>{'<'}</Button>
+                {this.state.activeDate.toDateString()}
+                <Button onClick={this.handleForward.bind(this)}>{'>'}</Button>
             </div>
         )
     }
